6ec1c64c6c5cbb62e34cdd77dfd09719a5c4efec
bautistaAD <aqbautista@up.edu.ph>
2023-07-04 09:55:11 +0800

    chore: Transfer logout button to navbar

--- frontend/src/pages/admin-dashboard.jsx
+++ frontend/src/pages/admin-dashboard.jsx
@@ -2,36 +2,14 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from 'react-bootstrap';
 import Cookies from 'universal-cookie';
+import Navbar from "../components/navbar";
 
 function AdminDashboard() {
   const username = localStorage.getItem("username");
-  const [isLoggedIn, setIsLoggedIn] = useState(true); // Set the initial state to `true`
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const cookies = new Cookies();
-    const authToken = cookies.get("authToken");
-
-    if (!authToken) {
-      setIsLoggedIn(false);
-      navigate("/login");
-    }
-  }, [navigate]);
-
-  const logout = () => {
-    const cookies = new Cookies();
-    cookies.remove("authToken");
-
-    localStorage.removeItem("username");
-
-    setIsLoggedIn(false);
-    navigate("/login");
-  };
 
   return (
     <div>
-      ADMIN DASHBOARD
-      <Button onClick={logout}>Logout</Button>
+      <Navbar displayName={username}></Navbar>
     </div>
   );
 }
