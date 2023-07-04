4435fd259dd723c0bc3fdf757462e0150b3b620b
bautistaAD <aqbautista@up.edu.ph>
2023-07-04 09:54:22 +0800

    feat: Add navigation bar

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,60 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/styles/navbar.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+import Cookies from 'universal-cookie';
+
+
+function Navbar(prop) {
+  const displayName = prop.displayName;
+  const [isLoggedIn, setIsLoggedIn] = useState(true); // Set the initial state to `true`
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const cookies = new Cookies();
+    const authToken = cookies.get("authToken");
+
+    if (!authToken) {
+      setIsLoggedIn(false);
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  const logout = () => {
+    const cookies = new Cookies();
+    cookies.remove("authToken");
+
+    localStorage.removeItem("username");
+
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
+
+  return (
+    <nav class="navbar fixed-top">
+    <div className='menu-btn'>
+      <MenuOutlinedIcon className="menu-icon"/>
+    </div>
+     <div class="dropdown">
+        <a className="btn btn-secondary dropdown-toggle rounded-0" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <AccountCircleRoundedIcon fontSize='large'/>
+          {displayName.toUpperCase()}
+        </a>
+        <div className="dropdown-menu" >
+            <div className='dropdown-container'style={{display: 'flex', alignItems: 'center',flexWrap: 'wrap', }}>
+              <AccountCircleRoundedIcon fontSize='large'/>
+              {displayName.toUpperCase()}
+            </div>
+            <Button id="logout-btn" onClick={logout}><LogoutRoundedIcon/>Logout</Button>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
