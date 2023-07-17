44c2aee024eb341133ad7035f18c874a0f779c7f
bautistaAD <aqbautista@up.edu.ph>
2023-07-17 14:54:35 +0800

    chore: Update election model

--- backend/models/election.js
+++ backend/models/election.js
@@ -2,5 +2,12 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const ElectionSchema = new Schema({
-    election_name: {type: String, required: true}
+    election_name: {type: String, required: true},
+    start_date_time: {type: Date, required: true},
+    end_date_time: {type: Date, required: true},
+    is_results_open: {type: Boolean, required: true}
 })
+
+const Election = mongoose.model("Election", ElectionSchema);
+
+export { Election, ElectionSchema};
