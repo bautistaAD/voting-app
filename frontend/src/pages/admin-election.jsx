034f03f1330e38933de3b9fd92dd09cea2402441
bautistaAD <aqbautista@up.edu.ph>
2023-07-17 14:56:37 +0800

    feat: Add sidebar and  navbar to election  page

--- frontend/src/pages/admin-election.jsx
+++ frontend/src/pages/admin-election.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import '../assets/styles/sidebar.css';
+import '../assets/styles/admin-election.css';
+import Navbar from "../components/navbar";
+import Sidebar from "../components/sidebar";
+import AdminSidebarData from "../components/admin-sidebar-data"
+import { useState } from 'react';
 
 function AdminElection() {
+  const [sidebar, setSidebar] = useState("inactive");
+  const [main, setMain] = useState("main-inactive");
+  const [startDT, setStartDT] = useState();
+
+  const handleStartDT = (e) => {
+    setStartDT(e.target.value);
+  }
+
+  const handleSumbit = () => {
+    console.log(startDT);
+  }
+
   return (
-    <div>AdminElection</div>
+    <div id="main">
+      <Navbar setSidebar={setSidebar} sidebar={sidebar} main={setMain}/>
+      <div className="content-election">
+        <Sidebar sidebar={sidebar} sideData={AdminSidebarData}></Sidebar>
+        <div className={main}  id="admin-election-main" style={{marginTop:"60px"}}>
+          ADMIN ELECTION
+          <input type="datetime-local" onChange={handleStartDT}></input>
+          <button onClick={()=> {handleSumbit()}}>Sumbit</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
