c38752af196a551caeab26f19205100706a090f3
bautistaAD <aqbautista@up.edu.ph>
2023-07-29 09:53:13 +0800

    feat: Add checker

--- frontend/src/components/buttons/header-btn-blue.js
+++ frontend/src/components/buttons/header-btn-blue.js
@@ -6,12 +6,11 @@ const HeaderBtnBlue = (prop) => {
   const btnName = prop.name;
   const Icon = prop.icon;
   const onClick = prop.onClick;
+  const checker = prop.checker;
   return (
     <div> 
-      <Button className="header-blue-btn btn-primary" onClick={onClick}>
-        <Icon/>
-        {btnName}
-      </Button>
+      {checker ? (<Button className="header-blue-btn btn-primary" onClick={onClick} disabled><Icon/>{btnName}</Button>) 
+      :(<Button className="header-blue-btn btn-primary" onClick={onClick}><Icon/>{btnName}</Button>) }
     </div>
   )
 }
