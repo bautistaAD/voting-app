5e90f966a45b4b8aedab2dc9f7352794fc807f45
bautistaAD <aqbautista@up.edu.ph>
2024-01-05 20:37:51 +0800

    feat: Create dashboard cards

---
+++ frontend/src/components/dashboard-card.js
@@ -0,0 +1,19 @@
+// import Card from 'react-bootstrap/Card';
+
+const DashboardCard = (prop) => {
+  const name = prop.name;
+  const png = prop.png
+  const count = prop.count
+  const iconClass = prop.iconClass
+    return (
+      <div className="dashboard-card-base shadow mb-5 bg-body rounded">
+        <div className={iconClass}>
+          <img src={png} className="dashboard-card-image" alt="none"></img>
+        </div>
+        <h3 className="dashboard-card-count">{count}</h3>
+        <div className="dashboard-card-title">{name}</div>
+      </div>
+    );
+  }
+  
+  export default DashboardCard;
--- frontend/src/pages/admin-dashboard.jsx
+++ frontend/src/pages/admin-dashboard.jsx
@@ -4,10 +4,29 @@ import '../assets/styles/sidebar.css';
 import '../assets/styles/admin-dashboard.css';
 import Sidebar from "../components/sidebar";
 import AdminSidebarData from "../components/admin-sidebar-data"
+import DashboardCard from "../components/dashboard-card";
+import membersPng from "../assets/images/group.png";
+import electionPng from "../assets/images/election.png";
 
 function AdminDashboard() {
   const [sidebar, setSidebar] = useState("inactive");
-  const [main, setMain] = useState("main-inactive")
+  const [main, setMain] = useState("main-inactive");
+  const [electionCount, setElectionCount] = useState()
+  const [memberCount, setMemberCount] = useState()
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/get-election-count')
+    .then((response) => response.json())
+    .then((body) =>{
+      setElectionCount(body)
+    })
+
+    fetch('http://localhost:3001/get-members-count')
+    .then((response) => response.json())
+    .then((body) =>{
+      setMemberCount(body)
+    })
+  }, [electionCount, memberCount])
 
   return (
     <div id="main">
@@ -15,7 +34,17 @@ function AdminDashboard() {
       <div className="content-dashboard">
         <Sidebar sidebar={sidebar} sideData={AdminSidebarData}></Sidebar>
         <div className={main} style={{marginTop:"60px"}}>
-          JUMBO HATDOG KAYA MO BA TO
+          <div className="dashboard-base">
+            <div className="dashboard dashboard-left">
+                <div className="dashboard-left-top">
+                  <DashboardCard png={membersPng} name={"Current Members"} count={memberCount} iconClass={"dashboard-card-icon icon-blue rounded-circle"}/>
+                  <DashboardCard png={electionPng} name={"Elections"} count={electionCount} iconClass={"dashboard-card-icon icon-orange rounded-circle"}/>
+                </div>
+            </div>
+            <div className="dashboard dashboard-right">
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
